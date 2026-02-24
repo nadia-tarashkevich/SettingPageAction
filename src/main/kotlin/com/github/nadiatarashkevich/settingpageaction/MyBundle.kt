dd715fda962af581b071d2125fa47b26cf558ef3
GitHub Action <action@github.com>
2026-02-24 05:47:23 +0000

    Template cleanup

--- src/main/kotlin/com/github/nadiatarashkevich/settingpageaction/MyBundle.kt
+++ src/main/kotlin/com/github/nadiatarashkevich/settingpageaction/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.nadiatarashkevich.settingpageaction
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/nadiatarashkevich/settingpageaction/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/nadiatarashkevich/settingpageaction/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.nadiatarashkevich.settingpageaction.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.nadiatarashkevich.settingpageaction.MyBundle
+import com.github.nadiatarashkevich.settingpageaction.services.MyProjectService
 import javax.swing.JButton
 
 
